8a94a14c8ed0e4f83e40c6fdfbe965a2063a3eeb
hartleys <stephen.hartley.phd@gmail.com>
2019-12-09 13:39:28 -0500

    0.1.5

--- build/buildScripts.bash
+++ build/buildScripts.bash
@@ -8,7 +8,8 @@ buildScripts/build.bash dnaNexusTutorial.SWH.2019.09.Rmd
 cp -R _book/* ../docs/
 
 cd ../docs/
-ln -s "1-MAINTITLE.html"  "index.html"
+rm index.html
+cp "1-MAINTITLE.html"  "index.html"
 cd ../build/
 ln -s ./_book/1-MAINTITLE.html  ./_book/index.html
 ln -s ./_book/1-MAINTITLE.html  ./_book/00index.html
